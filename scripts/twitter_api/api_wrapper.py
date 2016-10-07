477daf944a199a14ff2cb6f60f76a6a3b44022d9
Bruno Bachmann <bfbachmann@gmail.com>
2016-10-07 11:14:10 -0700

    added initialization to twitter api wrapper and changed api_keys format to yml for easy parsing, updated gitignore to reflext changes

--- scripts/twitter_api/api_wrapper.py
+++ scripts/twitter_api/api_wrapper.py
@@ -1,3 +1,19 @@
-import twitter
+# Check https://github.com/bear/python-twitter for docs on twitter package
 
-api = twitter.Api(consumer_key='', consumer_secret='', access_token_key='',  access_token_secret='')
+import twitter
+import pprint
+import yaml
+
+# load the API keys from api_keys.txt
+with open("api_keys.yml", 'r') as stream:
+    try:
+        keys = yaml.load(stream)
+    except yaml.YAMLError as exc:
+        print(exc)
+
+api = twitter.Api(consumer_key=keys['consumer_key'], consumer_secret=keys['consumer_secret'], access_token_key=keys['access_token_key'],  access_token_secret=keys['access_token_secret'])
+
+searchTerm = "hello"
+tweets = api.GetSearch(term=searchTerm)
+pp = pprint.PrettyPrinter(indent=4)
+pp.pprint(tweets)
