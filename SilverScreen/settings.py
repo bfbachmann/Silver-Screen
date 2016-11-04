83fa978f65d1eae37e6bb109b33491bb4787b676
peterdeutsch <peterdeutsch42@gmail.com>
2016-11-03 20:35:29 -0700

    Fixed databases line

--- SilverScreen/settings.py
+++ SilverScreen/settings.py
@@ -79,6 +79,14 @@ WSGI_APPLICATION = 'SilverScreen.wsgi.application'
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
 import dj_database_url
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
 DATABASES['default'] = dj_database_url.config()
 
 # Password validation
