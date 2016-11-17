697957148af195970068f10036bcd9218772dba8
liesl4 <liesl.s94@gmail.com>
2016-11-17 12:00:20 -0800

    couple small fixes for about page

--- app/urls.py
+++ app/urls.py
@@ -8,7 +8,7 @@ from . import views
 urlpatterns = [
     url(r'^index', views.index, name='index'),
     url(r'^$', views.index, name='index'),
-    url(r'^about', views.about, name='about')
+    url(r'^about', views.about, name='about'),
     url(r'^results', views.get_results_page, name='results'),
     url(r'^get_data', views.results, name='get_data'),
     url(r'^hide_trending_movie', views.hide_trending_movie, name='hide_trending_movie'),
